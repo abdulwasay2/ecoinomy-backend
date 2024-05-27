2e772b08b8e1fbbce931160cbc23e6aefe1f7866
abdul wasay <syedwasayali9@gmail.com>
2024-05-27 17:35:52 +0500

    chore: docker configs

--- config/ecoinomy/on-container-start.sh
+++ config/ecoinomy/on-container-start.sh
@@ -4,11 +4,10 @@
 # python manage.py makemigrations
 
 # Migrate created migrations to database
-# python manage.py migrate
+python manage.py migrate
 
 python manage.py collectstatic --noinput &&\
 
 # Start gunicorn server at port 8000 and keep an eye for app code changes
 # If changes occur, kill worker and start a new one
-# gunicorn --reload medical_advice.wsgi:application --timeout 120 -b 0.0.0.0:8000
 uvicorn --host 0.0.0.0 --port 8000 --reload --proxy-headers ecoinomy.asgi:application
