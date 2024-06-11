93efdc14a12366a2b1fc57170b08a3d0dc7a3304
abdulwasay <syedwasayali9@gmail.com>
2024-06-11 16:45:42 +0500

    change

--- ecoinomy/category/filters.py
+++ ecoinomy/category/filters.py
@@ -3,7 +3,12 @@ from .models import Category
 
 
 class CategoryFilter(filters.FilterSet):
+    is_parent = filters.CharFilter(method='is_parent')
 
     class Meta:
         model = Category
-        fields = ["parent_category", "name", "is_active"]
+        fields = ["parent_category", "name", "is_active", "is_parent"]
+
+    @staticmethod
+    def is_parent(queryset, name, value):
+        return queryset.filter(parent_category__isnull=value)
