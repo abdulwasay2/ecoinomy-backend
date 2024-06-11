3e685ee220a4736440b6164f6f9be9e70d452e35
abdulwasay <syedwasayali9@gmail.com>
2024-06-11 12:23:35 +0500

    adding static dir paths

--- ecoinomy/ecoinomy/settings/common.py
+++ ecoinomy/ecoinomy/settings/common.py
@@ -225,7 +225,11 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
