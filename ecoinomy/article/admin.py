d9480285604d52f5282a545a9280d6d06f5c89df
abdulwasay <syedwasayali9@gmail.com>
2024-06-11 12:55:25 +0500

    admin panel changes

--- ecoinomy/article/admin.py
+++ ecoinomy/article/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class StoreMediaItemInline(admin.StackedInline):
+    model = ArticleViews
+
+
+@admin.register(Article)
+class StoreAdmin(admin.ModelAdmin):
+    inlines = [StoreMediaItemInline]
+    list_display = ["id", "heading", "sub_category", "estimated_time_to_read", "article_type"]
+    list_display_links = ["heading"]
+
+
+@admin.register(Snippet)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ["id", "heading", "code", "description", "status"]
+    list_display_links = ["heading"]
--- ecoinomy/category/admin.py
+++ ecoinomy/category/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, CarousalItem
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "is_active"]
+    list_display_links = ["name"]
+
+
+@admin.register(CarousalItem)
+class CarousalItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "description", "is_active"]
