2867e27410a04f4a2a90cf05bc547d434b9eb16e
abdulwasay <syedwasayali9@gmail.com>
2024-06-12 12:47:17 +0500

    chore: adding some fields

--- ecoinomy/category/models.py
+++ ecoinomy/category/models.py
@@ -10,6 +10,7 @@ class Category(BaseModel):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     icon = models.FileField(upload_to="category/icon/", blank=True, null=True)
+    back_ground_image = models.FileField(upload_to="category/back_ground_image/", blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
 
@@ -17,3 +18,5 @@ class CarousalItem(BaseModel):
     description = models.TextField(blank=True)
     media = models.FileField(upload_to="category/icon/", blank=True, null=True)
     is_active = models.BooleanField(default=True)
+    article_id = models.OneToOneField("article.Article", on_delete=models.SET_NULL, blank=True, null=True)
+    snippet_id = models.OneToOneField("article.Snippet", on_delete=models.SET_NULL, blank=True, null=True)
