1f1988111586a443470d7eab6a18316b89c77c77
abdul wasay <syedwasayali9@gmail.com>
2024-06-24 01:49:14 +0500

    change: admin panel only to super admins

--- ecoinomy/eco_auth/admin.py
+++ ecoinomy/eco_auth/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+def has_superuser_permission(request):
+    return request.user.is_active and request.user.is_superuser
+
+# Only active superuser can access root admin site (default)
+admin.site.has_permission = has_superuser_permission
