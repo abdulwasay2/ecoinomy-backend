34ebedc2ade0cee4667501d65e6703f1e27efa72
abdul wasay <syedwasayali9@gmail.com>
2024-06-24 01:19:27 +0500

    change: minor change

--- ecoinomy/ecoinomy/settings/development.py
+++ ecoinomy/ecoinomy/settings/development.py
@@ -5,3 +5,5 @@ from ecoinomy.settings import env
 SECRET_KEY = env('SECRET_KEY')
 OTP_SECRET_KEY = env('OTP_SECRET_KEY')
 PASSWORD_RESET_OTP_SECRET = env('PASSWORD_RESET_OTP_SECRET')
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000", "http://ec2-51-20-128-52.eu-north-1.compute.amazonaws.com"]
