177ef3c0ba1a9b22afc09292a8a80d9d00853f73
abdul wasay <syedwasayali9@gmail.com>
2024-07-09 17:00:34 +0500

    chore: minor change

--- ecoinomy/article/filters.py
+++ ecoinomy/article/filters.py
@@ -8,10 +8,11 @@ class ArticleFilter(filters.FilterSet):
     created = filters.DateFromToRangeFilter(label='Created Date Range')
     heading = filters.CharFilter(lookup_expr='icontains')
     sub_category = filters.NumberFilter(method="filter_by_category_id")
+    # is_active = filters.BooleanFilter(method="filter_by_is_active")
 
     class Meta:
         model = Article
-        fields = ["sub_category", "heading", "article_type", "article_by", "created"]
+        fields = ["sub_category", "heading", "article_type", "article_by", "created", "is_active"]
 
     @staticmethod
     def filter_by_category_id(queryset, name, value):
@@ -19,6 +20,13 @@ class ArticleFilter(filters.FilterSet):
             Q(sub_category_id=value) |
             Q(sub_category__parent_category_id=value)
             )
+    
+    # @staticmethod
+    # def filter_by_is_active(queryset, name, value):
+    #     return queryset.filter(
+    #         Q(is_active=value) |
+    #         Q(sub_category__parent_category_id=value)
+    #         )
     
 
 class SnippetFilter(filters.FilterSet):
@@ -28,7 +36,7 @@ class SnippetFilter(filters.FilterSet):
 
     class Meta:
         model = Snippet
-        fields = ["sub_category", "heading", "created"]
+        fields = ["sub_category", "heading", "created", "is_active"]
 
     @staticmethod
     def filter_by_category_id(queryset, name, value):
